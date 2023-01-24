0fbdb0089e9c4161a86195edd42e82563ebec55d
jlakshmibhavani <119418970+jlakshmibhavani@users.noreply.github.com>
2023-01-24 20:53:13 +0530

    Add files via upload

---
+++ THISkey.java
@@ -0,0 +1,13 @@
+class A{
+    A(){
+        this(5);
+        System.out.println("hello a");
+    }
+    A(int x){
+        System.out.println(x);
+    }
+}
+class TestThis6{
+    public static void main(String args[]){
+        A a=new A();
+    }}  
